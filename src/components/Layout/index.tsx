85c55d83ef16bde82770c84505236515dc408970
DanielMartinez <daniel.martinez@volcanicinternet.com>
2023-01-25 17:40:15 +0100

    feat/tech-interview (pages): add SEO metatags

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,6 +6,32 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>Komon Tech Interview Frontend</title>
+
+        <meta name="title" content="Komon Tech Interview Frontend" />
+        <meta
+          name="description"
+          content="Next JS project made by Daniel Martinez for the Komon Tech Interview"
+        />
+        <meta name="author" content="Daniel Martinez" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="#" />
+        <meta property="og:title" content="Komon Tech Interview Frontend" />
+        <meta
+          property="og:description"
+          content="Next JS project made by Daniel Martinez for the Komon Tech Interview"
+        />
+        <meta property="og:image" content="#" />
+        <meta property="twitter:card" content="#" />
+        <meta property="twitter:url" content="#" />
+        <meta
+          property="twitter:title"
+          content="Komon Tech Interview Frontend"
+        />
+        <meta
+          property="twitter:description"
+          content="Next JS project made by Daniel Martinez for the Komon Tech Interview"
+        />
+        <meta property="twitter:image" content="#" />
       </Head>
 
       <section className="shadow-lg min-h-screen">
