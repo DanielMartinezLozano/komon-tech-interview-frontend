bc13f584a3a5e23dec54d3a06a87e69d8591d48a
DanielMartinez <daniel.martinez@volcanicinternet.com>
2023-01-26 19:46:35 +0100

    feat/tech-interview (/core): add constants and env variables

--- src/core/server-side.ts
+++ src/core/server-side.ts
@@ -1,7 +1,10 @@
+import { API_ENDPOINT_CONSTANTS } from "./constants/constants";
+
 export const getPosts = async () => {
   // API call to our own fake API
-  // This URL should be placed inside an .env file
-  const url = "http://localhost:3001/api/posts";
+  const url = process.env.NEXT_PUBLIC_API_URL
+    ? `${process.env.NEXT_PUBLIC_API_URL}/${API_ENDPOINT_CONSTANTS.posts}`
+    : "http://localhost:3000/api/posts";
   const response = await fetch(url).then((res) => res.json());
 
   return {
