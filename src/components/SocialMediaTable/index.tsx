4418e3e742f689d367875c49834e6ac52bf5f965
DanielMartinez <daniel.martinez@volcanicinternet.com>
2023-01-26 19:44:39 +0100

    feat/tech-interview (SocialMediaTable, Login, ContentPage): add login page and restrict options without signing in

--- src/components/SocialMediaTable/index.tsx
+++ src/components/SocialMediaTable/index.tsx
@@ -3,6 +3,7 @@ import ChevronIcon from "../icons/ChevronIcon";
 import DiscordIcon from "../icons/DiscordIcon";
 import InstagramIcon from "../icons/InstagramIcon";
 import TwitterIcon from "../icons/TwitterIcon";
+import LoginModal from "../LoginModal";
 
 const SocialMediaTable = () => {
   const [socialMediaSelected, setSocialMediaSelected] =
@@ -34,36 +35,10 @@ const SocialMediaTable = () => {
       <h2>Social Media</h2>
       <ul>
         {socialMediaSelected ? (
-          <div className="grid gap-6">
-            <span className="flex items-center gap-2 pt-4">
-              <button onClick={() => handleSelectSocialMedia(null)}>
-                <ChevronIcon />
-              </button>
-              <h3>Sign in with {socialMediaSelected.text}</h3>
-              {socialMediaSelected.icon}
-            </span>
-            <input
-              data-testid="input-email"
-              type="email"
-              id="email"
-              className="bg-dark-05 text-dark-20 focus:text-dark-75 p-3 pl-10 h-11  border-dark-20 border border-solid text-sm rounded-lg leading-tight font-euclid-triangle block w-full placeholder-asphalt"
-              placeholder="Username or E-mail"
-            />
-            <input
-              data-testid="input-password"
-              type="password"
-              id="password"
-              className="bg-dark-05 text-dark-20 focus:text-dark-75 p-3 pl-10 h-11  border-dark-20 border border-solid text-sm rounded-lg leading-tight font-euclid-triangle block w-full placeholder-asphalt"
-              placeholder="Password"
-            ></input>
-            <button
-              type="button"
-              data-testid="user-login-module-login-button"
-              className="w-full text-white bg-black border border-transparent hover:bg-drak-75 focus:ring-drak-20 disabled:hover:bg-dark-05 focus:ring-4 group flex items-center justify-center py-2 text-center font-medium focus:z-10 rounded-lg dark:border-solid dark:border-white font-euclid-triangle"
-            >
-              Log in
-            </button>
-          </div>
+          <LoginModal
+            socialMediaSelected={socialMediaSelected}
+            handleSelectSocialMedia={handleSelectSocialMedia}
+          />
         ) : (
           socialMediaPlatforms.map((platform) => (
             <li key={platform.text} className="my-6">
--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,15 +1,24 @@
+import { useUserContext } from "context/AuthContext";
 import { getPosts } from "core/server-side";
 import Image from "next/image";
 
 const Content = ({ posts }: ContentPageProps) => {
+  const user = useUserContext();
+
   return (
     <>
       <h1 className="my-6">Content</h1>
+      {!user && (
+        <p className="my-20 bold text-lg text-center">
+          Sign in to any social media to view your content
+        </p>
+      )}
       <ul
         className="grid grid-cols-3 gap-x-4 gap-y-8"
         data-testid="content-post-list"
       >
-        {posts.length > 0 &&
+        {user &&
+          posts.length > 0 &&
           posts.map((post) => (
             <li key={post.id}>
               <div>
